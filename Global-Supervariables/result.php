883f5717080e0bc487770f4a88911660c21f88ea
awvdeker <alexander.vandekerckhove@gmail.com>
2019-05-13 16:28:23 +0200

    does GET work?

---
+++ Global-Supervariables/result.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+<p>Variables sent via POST<p>
+<p>Top 5 Tv shows</p>
+<p>Top 5 Movies</p>
+<br/>
+<p>Variables sent via GET<p>
+<p>Favourite Country : <?php echo $_GET["favouritecountry"]; ?></p>
+<p>Worst movie ever seen : <?php echo $_GET["worstmovie"]; ?></p>
+
+</body>
+</html>
---
+++ Global-Supervariables/setup.php
@@ -0,0 +1,25 @@
+<form action="result.php" method="post">
+  <p>Top 5 Tv shows</p>
+    <ol>
+      <li><input type="text" name="tvshow1"/></li>
+      <li><input type="text" name="tvshow2"/></li>
+      <li><input type="text" name="tvshow3"/></li>
+      <li><input type="text" name="tvshow4"/></li>
+      <li><input type="text" name="tvshow5"/></li>
+    </ol>
+  <p>Top 5 Movies</p>
+  <ol>
+    <li><input type="text" name="movie1"/></li>
+    <li><input type="text" name="movie2"/></li>
+    <li><input type="text" name="movie3"/></li>
+    <li><input type="text" name="movie4"/></li>
+    <li><input type="text" name="movie5"/></li>
+  </ol>
+  <input type="submit" name="submit" value="submit"/>
+</form>
+
+<form action="result.php" method="get">
+  <p>Favourite country<input type="text" name="favouritecountry"/></p>
+  <p>Worst movie ever seen<input type="text" name="worstmovie"/></p>
+  <input type="submit" name="submit" value="submit"/>
+</form>
