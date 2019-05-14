2c053606271446840185964a2be29ed01b1421b3
awvdeker <alexander.vandekerckhove@gmail.com>
2019-05-14 17:07:55 +0200

    $_SERVER variables shown

--- Global-Supervariables/result.php
+++ Global-Supervariables/result.php
@@ -5,6 +5,7 @@
 </head>
 <body>
 <div class="container">
+  <!-- $_POST and $_GET -->
   <p>Variables sent via POST<p>
   <p>Top 5 Tv shows</p>
   <ol>
@@ -26,6 +27,25 @@
   <p>Variables sent via GET<p>
   <p>Favourite Country : <?php echo $_GET["favouritecountry"]; ?></p>
   <p>Worst movie ever seen : <?php echo $_GET["worstmovie"]; ?></p>
+  <br/>
+
+  <!-- //$_SERVER -->
+  <p>$_SERVER variables :</p>
+  <br/>
+  <p>$_SERVER['SCRIPT_NAME'] --> <?php echo $_SERVER['SCRIPT_NAME']; ?></p>
+  <p>$_SERVER['HTTP_HOST'] --> <?php echo $_SERVER['HTTP_HOST']; ?></p>
+  <p>$_SERVER['PHP_SELF'] --> <?php echo $_SERVER['PHP_SELF']; ?></p>
+  <p>$_SERVER['SERVER_ADDR'] --> <?php echo $_SERVER['SERVER_ADDR']; ?></p>
+  <p>$_SERVER['SERVER_NAME'] --> <?php echo $_SERVER['SERVER_NAME']; ?></p>
+  <p>$_SERVER['REMOTE_ADDR'] --> <?php echo $_SERVER['REMOTE_ADDR']; ?></p>
+  <p>$_SERVER['REMOTE_HOST'] --> <?php echo $_SERVER['REMOTE_HOST']; ?></p>
+  <p>$_SERVER['REMOTE_PORT'] --> <?php echo $_SERVER['REMOTE_PORT']; ?></p>
+  <p>$_SERVER['SCRIPT_FILENAME'] --> <?php echo $_SERVER['SCRIPT_FILENAME']; ?></p>
+  <p>$_SERVER['SERVER_PORT'] --> <?php echo $_SERVER['SERVER_PORT']; ?></p>
+  <p>$_SERVER['SCRIPT_URI'] --> <?php echo $_SERVER['SCRIPT_URI']; ?></p>
+  <br/>
+
+
 </div>
 </body>
 </html>
