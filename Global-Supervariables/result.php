4afcb77ac89d36dabdac6cf44460e65fa750d90b
awvdeker <alexander.vandekerckhove@gmail.com>
2019-05-14 10:58:35 +0200

    added css

--- Global-Supervariables/result.php
+++ Global-Supervariables/result.php
@@ -1,5 +1,10 @@
 <html>
+<head>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css">
+</head>
 <body>
+<div class="container">
   <p>Variables sent via POST<p>
   <p>Top 5 Tv shows</p>
   <ol>
@@ -21,5 +26,6 @@
   <p>Variables sent via GET<p>
   <p>Favourite Country : <?php echo $_GET["favouritecountry"]; ?></p>
   <p>Worst movie ever seen : <?php echo $_GET["worstmovie"]; ?></p>
+</div>
 </body>
 </html>
--- Global-Supervariables/setup.php
+++ Global-Supervariables/setup.php
@@ -1,5 +1,12 @@
+<html>
+<head>
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="container">
 <form action="result.php" method="post">
-  <p>Top 5 Tv shows</p>
+  <p>Top 5 Tv shows (POST)</p>
     <ol>
       <li><input type="text" name="tvshow1"/></li>
       <li><input type="text" name="tvshow2"/></li>
@@ -7,7 +14,7 @@
       <li><input type="text" name="tvshow4"/></li>
       <li><input type="text" name="tvshow5"/></li>
     </ol>
-  <p>Top 5 Movies</p>
+  <p>Top 5 Movies (POST)</p>
   <ol>
     <li><input type="text" name="movie1"/></li>
     <li><input type="text" name="movie2"/></li>
@@ -19,7 +26,10 @@
 </form>
 
 <form action="result.php" method="get">
-  <p>Favourite country<input type="text" name="favouritecountry"/></p>
-  <p>Worst movie ever seen<input type="text" name="worstmovie"/></p>
+  <p>Favourite country (GET) <input type="text" name="favouritecountry"/></p>
+  <p>Worst movie ever seen (GET) <input type="text" name="worstmovie"/></p>
   <input type="submit" name="submit" value="submit"/>
 </form>
+</div>
+</body>
+</html>
