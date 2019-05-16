ec2625f8dce88b5ab64f477197a3972b2f77dbeb
awvdeker <alexander.vandekerckhove@gmail.com>
2019-05-16 16:10:23 +0200

    loops exercise

---
+++ Loops/loop.php
@@ -0,0 +1,35 @@
+<?php
+
+$array = [
+	["tv-show" => "FC De Kampioenen", "rating" => 1],
+	["tv-show" => "Bevergem", "rating" => 5],
+	["tv-show" => "Temptation Island", "rating" => 1],
+  ["tv-show" => "De slimste mens", "rating" => 2],
+  ["tv-show" => "In de gloria", "rating" => 4],
+	["tv-show" => "Alles kan beter", "rating" => 4],
+	["tv-show" => "Het geslacht de Pauw", "rating" => 3],
+	["tv-show" => "Gert Late Night", "rating" => 4]
+];
+
+?>
+
+<table>
+<tr>
+  <th>tv-show</th>
+  <th>rating</th>
+</tr>
+
+<?php
+foreach($array as $arr):
+?>
+
+<tr>
+	<td><?php echo $arr["tv-show"]; ?></td>
+	<td><?php echo $arr["rating"]; ?></td>
+</tr>
+
+<?php
+endforeach;
+?>
+
+</table>
