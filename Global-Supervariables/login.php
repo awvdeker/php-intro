46762fe28212d05aa3637e8ef7e5c1664a7563c3
awvdeker <alexander.vandekerckhove@gmail.com>
2019-05-16 14:38:30 +0200

    username and password

---
+++ Global-Supervariables/login.php
@@ -0,0 +1,14 @@
+<html>
+<head>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+<div class="container">
+  <form action="result.php" method="post">
+    <p>Username <input type="text" name="username"/></p>
+    <p>Password <input type="text" name="password"/></p>
+    <input type="submit" name="submit" value="submit"/>
+  </form>
+</div>
+</body>
+</html>
--- Global-Supervariables/result.php
+++ Global-Supervariables/result.php
@@ -8,7 +8,18 @@
   <link rel="stylesheet" href="style.css">
 </head>
 <body>
+
+  <?php
+    $username = "alex";
+    $password = "pass";
+
+    if (($_POST["username"] === $username) && ($_POST["password"] === $password)){
+    echo "Hello $username";
+  ?>
+
+
 <div class="container">
+
   <!-- tabs with javascript plug in to display certain divs -->
   <ul class="nav nav-tabs" id="myTab" role="tablist">
     <li class="nav-item">
@@ -159,5 +170,12 @@
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+<?php
+}else {
+  echo "Wrong password or username";
+}
+?>
+
 </body>
 </html>
