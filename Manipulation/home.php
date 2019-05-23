274a38809d62d72bb303b7df3afa96fc968dd0a1
awvdeker <alexander.vandekerckhove@gmail.com>
2019-05-23 10:35:58 +0200

    arr, ass arr, obj

---
+++ Manipulation/home.php
@@ -0,0 +1,22 @@
+<?php
+$myArr=array("Samuel","Nathan","David","Simon");
+$myAssArr=array("Samuel"=>"35","Nathan"=>"33","David"=>"32","Simon"=>"30");
+class MyClass{
+  public $name;
+  public $age;
+}
+$nephewOne=new MyClass();
+$nephewOne->name="Samuel";
+$nephewOne->age="35";
+$nephewTwo=new MyClass();
+$nephewTwo->name="Nathan";
+$nephewTwo->age="33";
+$nephewThree=new MyClass();
+$nephewThree->name="David";
+$nephewThree->age="32";
+$nephewFour=new MyClass();
+$nephewFour->name="Simon";
+$nephewFour->age="30";
+
+
+?>
