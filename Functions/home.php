6753fc677a656968544483e00723f5a4b89f04a9
awvdeker <alexander.vandekerckhove@gmail.com>
2019-05-24 17:05:34 +0200

    initial commit

---
+++ Functions/home.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div class="box">
+    <button type="button">Generate object</button>
+  </div>
+  <div class="box">
+    <button type="button">Revert object</button>
+  </div>
+  <div class="box">
+    <input type="text" name="username"/>
+    <button type="button">Get a nickname</button>
+  </div>
+</body>
